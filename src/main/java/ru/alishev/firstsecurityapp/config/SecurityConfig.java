8c27cb09dbb0f710e641376552132d324e477de6
Evgeniy <ifzy12@gmail.com>
2023-10-29 13:13:15 +0300

    custom auth page for spring security

--- src/main/java/ru/alishev/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/firstsecurityapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.alishev.firstsecurityapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -25,6 +26,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //auth.authenticationProvider(authProvider);
         auth.userDetailsService(personDetailsService);
         // Spring сам найдет человека и сравнит его данные с данными из формы
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Конфигурируем сам Spring Security
+        // Сюда поступает HTTP запрос
+        // Сами данные с формы мы не обрабатываем, за нас это сделает Spring Security
+
+        // Конфигурируем авторизацию - считается сверху - вниз, от более детальных к менее
+
+        http.csrf().disable() // Отключаем защиту от межсайтовой подделки запросов
+                .authorizeRequests() // Включаем авторизацию на доступ к страницам
+                .antMatchers("/auth/login", "/error").permitAll() // Запросы на эти страницы доступны всем
+                .anyRequest().authenticated()
+                .and() // Была настройка авторизации, теперь настройка страницы логина
+                .formLogin().loginPage("/auth/login") // Страница формы авторизации
+                .loginProcessingUrl("/process_login") // Указываем адрес по которому Spring будет ждать данные
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/auth/login?error");
     }
 
     @Bean
